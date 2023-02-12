7424dca57602a253820f57617499da5dc79d9fd0
cvrocha <cvrochaa13@gmail.com>
2023-02-12 18:59:07 -0300

    ==COMMIT WEBSITE

---
+++ assets/js/app.js
@@ -0,0 +1,15 @@
+const shareButtons = document.querySelectorAll('.tile-share-button')
+console.log(shareButtons)
+
+async function copyText(e) {
+//prevent button going to the site
+    e.preventDefault()
+    const link = this.getAttribute('link')
+    console.log(link)
+    try {
+        await navigator.clipboard.writeText(link)
+        alert("Copied the text: " + link)
+    } catch (err) {
+        console.error(err)
+    }
+}
