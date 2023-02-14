0b454b73c9cda5e5cc3971032b8dd98bff31f74c
cvrocha <cvrochaa13@gmail.com>
2023-02-13 22:29:59 -0300

    ATT

--- assets/js/background.js
+++ assets/js/background.js
@@ -1,6 +1,11 @@
-function Randomize() {
-  var images = new Array("/images/tema/header/IMG2.jpg", "/images/tema/header/IMG.jpg");
-  var imageNum = Math.floor(Math.random() * images.length);
-  document.getElementById("background").style.backgroundImage = "url('" + images[imageNum] + "')";
-}
-window.onload = Randomize;
+// let styleElem = document.head.appendChild(document.createElement("style"));
+  
+//       styleElem.innerHTML = "#theDiv:before {content: '';position: fixed;top: 0;left: 0;height: 100%;width: 100%;z-index: -1;background: rgba(204, 204, 204, 0.2);background-size: cover;background-repeat: no-repeat;}";
+      
+//       function Randomize() {
+//         let images = new Array("https://wallpaperaccess.com/full/2073407.jpg", "https://www.losangelesdream.com/wp-content/uploads/2018/01/california-los-angeles-4k-1536x960.jpeg", "https://4kwallpapers.com/images/walls/thumbs_3t/7284.jpg", "https://wallup.net/wp-content/uploads/2019/09/340255-sao-paulo-city-megapole-south-america-brazil-buildings-1-748x506.jpg");
+//         let imageNum = Math.floor(Math.random() * images.length);
+        
+//         document.getElementById("theDiv").style.backgroundImage = "url('" + images[imageNum] + "')";
+//       }
+//       window.onload = Randomize;
